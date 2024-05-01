cd7be423ea9cfc8d5886942a4e6f91de4c6df5ba
karina <nkvalenciapi@cesde.net>
2024-04-30 19:31:42 -0500

    Se agrega SD de firebase

---
+++ src/components/config/firebaseConfig.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyNH2WiPputZn5ehA5TwWfUUhyVG_4DpU",
+  authDomain: "app-firebase-martes-jzv.firebaseapp.com",
+  projectId: "app-firebase-martes-jzv",
+  storageBucket: "app-firebase-martes-jzv.appspot.com",
+  messagingSenderId: "1085568341646",
+  appId: "1:1085568341646:web:69dec805f76df9c3591bdf",
+};
+
+const app = initializeApp(firebaseConfig);
