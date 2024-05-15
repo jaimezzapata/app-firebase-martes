a133cfa183c4205205fc648cb9dd691a2182eaaf
karina <nkvalenciapi@cesde.net>
2024-05-14 20:57:51 -0500

    Se implementa busqueda de usuarios en firestore

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -5,6 +5,9 @@ import { collection, getDocs } from "firebase/firestore";
 
 const Login = () => {
   const [usuarios, setUsuarios] = useState();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
   async function getUsuarios() {
     let resultado = collection(initFirestore, "usuarios");
     let data = await getDocs(resultado);
@@ -13,11 +16,22 @@ const Login = () => {
     console.log(data.docs.map((doc) => ({ ...doc.data() })));
     setUsuarios(data.docs.map((doc) => ({ ...doc.data() })));
   }
-  useEffect(()=>{
+  useEffect(() => {
     getUsuarios();
-  },[])
-
-  // console.log(initFirestore);
+  }, []);
+  const buscarUsuario = () => {
+    let estado = usuarios.some(
+      (usuario) => usuario.user === user && usuario.password == password
+    );
+    return estado;
+  };
+  const iniciarSesion = () => {
+    if (buscarUsuario()) {
+      console.log("Bievenido usuario");
+    } else {
+      console.log("Error de credenciales");
+    }
+  };
   return (
     <div className="login-page">
       <div className="form">
@@ -31,9 +45,19 @@ const Login = () => {
           </p>
         </form>
         <form className="login-form">
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
-          <button type="button">login</button>
+          <input
+            onChange={(e) => setUser(e.target.value)}
+            type="text"
+            placeholder="username"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+          />
+          <button onClick={iniciarSesion} type="button">
+            login
+          </button>
           <p className="message">
             Not registered? <a href="#">Create an account</a>
           </p>
